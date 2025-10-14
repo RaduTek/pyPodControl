c3d574252730f4d4417648be6f74a5902203ce36
RaduTek <me@radutek.uk>
2025-10-14 11:16:06 +0300

    Improve simple remote test script

--- tests/simpleremote.py
+++ tests/simpleremote.py
@@ -1,6 +1,18 @@
 import sys
 from pypodcontrol import iAPClient, General, SimpleRemote
 
+if len(sys.argv) < 2:
+    print("Missing button argument")
+    print("Available buttons", SimpleRemote.buttons)
+    exit(1)
+
+button = sys.argv[1]
+
+if button not in SimpleRemote.buttons:
+    print("Unknown button:", button)
+    print("Available buttons", SimpleRemote.buttons)
+    exit(1)
+
 iap = iAPClient("/dev/ttyUSB0")
 
 g = General(iap)
